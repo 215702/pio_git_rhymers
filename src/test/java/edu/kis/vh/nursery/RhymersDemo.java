664dfc70540a3da54c0eb01e9bf7cfcb4563b9a9
215702 <kapis18@interia.pl>
2020-01-06 17:43:55 +0100

    1.5 Refaktoryzacja klasy RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,8 +8,13 @@ class RhymersDemo {
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
-		DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
+        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
+factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
         for (int i = 1; i < 15; i++) {
             for (int j = 0; j < 3; j++) {
@@ -27,7 +32,6 @@ class RhymersDemo {
             System.out.println();
         }
         System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
-
     }
 
 }
